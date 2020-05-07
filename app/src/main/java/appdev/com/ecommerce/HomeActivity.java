d751dcec4310b0e10dce0097e932e25092b96e0d
sau-skt <saurabhchatterjee18@gmail.com>
2020-05-08 01:06:22 +0530

    Uncomplete

--- app/src/main/java/appdev/com/ecommerce/HomeActivity.java
+++ app/src/main/java/appdev/com/ecommerce/HomeActivity.java
@@ -67,8 +67,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,CartActivity.class);
-                startActivity(intent);
+                if (!type.equals("Admin")){
+                    Intent intent = new Intent(HomeActivity.this,CartActivity.class);
+                    startActivity(intent);
+                }
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -168,24 +170,35 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.nav_cart){
-            Intent intent = new Intent(HomeActivity.this,CartActivity.class);
-            startActivity(intent);
+            if (!type.equals("Admin")){
+                Intent intent = new Intent(HomeActivity.this,CartActivity.class);
+                startActivity(intent);
+            }
         } else if (id == R.id.nav_search){
-            Intent intent = new Intent(HomeActivity.this,SearchProductsActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_categories){
+            if (!type.equals("Admin")){
+                Intent intent = new Intent(HomeActivity.this,SearchProductsActivity.class);
+                startActivity(intent);
+            }
 
+        } else if (id == R.id.nav_categories){
+            if (!type.equals("Admin")){
+
+            }
         } else if (id == R.id.nav_settings){
-            Intent intent = new Intent(HomeActivity.this,SettingsActivity.class);
-            startActivity(intent);
+            if (!type.equals("Admin")){
+                Intent intent = new Intent(HomeActivity.this,SettingsActivity.class);
+                startActivity(intent);
+            }
 
         } else if (id == R.id.nav_logout){
-            Paper.book().destroy();
+            if (!type.equals("Admin")){
+                Paper.book().destroy();
 
-            Intent intent = new Intent(HomeActivity.this,MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+                Intent intent = new Intent(HomeActivity.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
         }
         return true;
     }
